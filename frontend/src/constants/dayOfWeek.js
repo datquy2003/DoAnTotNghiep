86bc6ce7fc16b076edc3b967948d55fdcfa32480
Le Dat Quy <ilovekudo1170@gmail.com>
2025-12-15 00:24:13 +0700

    Feature/jobs post page

---
+++ frontend/src/constants/dayOfWeek.js
@@ -0,0 +1,9 @@
+export const DAY_OF_WEEK = {
+  1: "Thứ 2",
+  2: "Thứ 3",
+  3: "Thứ 4",
+  4: "Thứ 5",
+  5: "Thứ 6",
+  6: "Thứ 7",
+  7: "Chủ nhật",
+};
---
+++ frontend/src/constants/educationLevel.js
@@ -0,0 +1,9 @@
+export const EDUCATION_LEVEL = {
+  ALL: "Tất cả",
+  NO_REQUIRED: "Không yêu cầu",
+  HIGH_SCHOOL: "Cấp 3",
+  COLLEGE: "Cao đẳng",
+  UNIVERSITY: "Đại học",
+  POSTGRADUATE: "Thạc sĩ",
+  DOCTORAL: "Tiến sĩ",
+};
